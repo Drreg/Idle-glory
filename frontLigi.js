406d3ed6ff39f02a3636ebc2f7aca8ce30e2db8a
Drreg <98709682+Drreg@users.noreply.github.com>
2023-02-09 22:49:21 +0200

    Add front ligi

---
+++ frontLigi.js
@@ -0,0 +1,17 @@
+frontLigi = (n = 0) => {
+  if (n > 7) return;
+  WebConnect.getInstance().gameConnect.request("game.RatedBattleHandler.matchOpponent", {
+    token: InfoManager.instance.token,
+  }, function() {
+    WebConnect.getInstance().gameConnect.request("game.RatedBattleHandler.endRateFight", {
+      token: InfoManager.instance.token,
+      win: 1
+    }, function() {
+      setTimeout(() => {
+        frontLigi(n+1);
+      }, 1000*20)
+    });
+  });
+}
+
+frontLigi();
